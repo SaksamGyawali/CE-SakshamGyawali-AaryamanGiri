f91a69337c3d53241c52d19f407b6652399f7b6a
Saksahm <saksamgyawali12@gmail.com>
2024-05-15 06:19:45 +0100

    yes

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -7,10 +7,23 @@ class Node{
    int key;
    Node* left;
    Node* right;
+   Node* pred;
    Node(int data):key(data), left(nullptr),right(nullptr)  {}      
 };
 
+Node *Root;
+
 class LinkedBST: public BST {
+
+  public:
+  
+  bool isEmpty(){
+   if (Root->key==0)
+    return true;
+  }
+
+  void addBST(int key){
    
+  }
    
 };
