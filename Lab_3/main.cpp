13b04a9a60553fab4d5d11c38344d94a09de44d6
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-02 19:33:15 +0545

    Added main.cpp for linkedlistBST

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -1,36 +1,24 @@
-#include<iostream>
+#include "linked_bst.h"
+#include <iostream>
 using namespace std;
-#include "bst.h"
-#include "linkedlist.cpp"
 
-int main()
-{
-    LinkedBST bst;
+int main() {
+    InterfaceBST* bst = new LinkedBST();
+    cout << "Is the tree empty? " << (bst->isEmpty() ? "Yes" : "No") << endl;
 
-    // Test isEmpty()
-    cout << "Is the tree empty? " << (bst.isEmpty() ? "Yes" : "No") << endl;
+    bst->addBST(2);
+    bst->addBST(4);
+    bst->addBST(6);
+    bst->addBST(8);
+    bst->addBST(10);
+    bst->addBST(12);
 
-    // Test addBST()
-    bst.addBST(50);
-    bst.addBST(30);
-    bst.addBST(20);
-    bst.addBST(40);
-    bst.addBST(70);
-    bst.addBST(60);
-    bst.addBST(80);
+    cout << "Does the tree contain 10? " << (bst->searchBST(10) ? "Yes" : "No") << endl;
+    cout << "Does the tree contain 9? " << (bst->searchBST(9) ? "Yes" : "No") << endl;
 
-    // Test searchBST()
-    cout << "Does 40 exist in the tree? " << (bst.searchBST(40) ? "Yes" : "No") << endl;
-    cout << "Does 25 exist in the tree? " << (bst.searchBST(25) ? "Yes" : "No") << endl;
+    bst->removeBST(6);
+    cout << "Does the tree contain 6 after removal? " << (bst->searchBST(6) ? "Yes" : "No") << endl;
 
-    // Test removeBST()
-    cout << "Removing 20" << (bst.removeBST(20) ? ": Success" : ": Failure") << endl;
-    cout << "Removing 30" << (bst.removeBST(30) ? ": Success" : ": Failure") << endl;
-    cout << "Removing 50" << (bst.removeBST(50) ? ": Success" : ": Failure") << endl;
-    cout << "Does 50 exist in the tree after removal? " << (bst.searchBST(50) ? "Yes" : "No") << endl;
-
-    // Test isEmpty() after removals
-    cout << "Is the tree empty? " << (bst.isEmpty() ? "Yes" : "No") << endl;
-
+    delete bst;
     return 0;
 }
