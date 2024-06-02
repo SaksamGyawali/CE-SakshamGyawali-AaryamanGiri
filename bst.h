b4c31ddc3be16fdc09bad25a46010fa304adc375
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-02 04:46:17 +0100

    Removed duplicate files

--- bst.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-class BST{
-    public:
-        virtual bool isEmpty() = 0;
-        virtual void addBST(int data) = 0;
-        virtual bool removeBST(int data) = 0;
-        virtual bool searchBST(int targetKey) const = 0;
-};
