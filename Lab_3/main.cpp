5926e4355b27fcd91601531197220d90ab5298ed
SaksamGyawali <saksamgyawali12@gmail.com>
2024-06-02 20:22:50 +0545

    Done

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -1,23 +1,23 @@
-#include "linked_bst.h"
+#include "linkedlist.cpp"
 #include <iostream>
 using namespace std;
 
 int main() {
-    InterfaceBST* bst = new LinkedBST();
+    BST* bst = new LinkedBST();
     cout << "Is the tree empty? " << (bst->isEmpty() ? "Yes" : "No") << endl;
 
-    bst->addBST(2);
-    bst->addBST(4);
-    bst->addBST(6);
-    bst->addBST(8);
+    bst->addBST(22);
+    bst->addBST(44);
+    bst->addBST(66);
+    bst->addBST(80);
     bst->addBST(10);
     bst->addBST(12);
 
-    cout << "Does the tree contain 10? " << (bst->searchBST(10) ? "Yes" : "No") << endl;
+    cout << "Does the tree contain 10? " << (bst->searchBST(80) ? "Yes" : "No") << endl;
     cout << "Does the tree contain 9? " << (bst->searchBST(9) ? "Yes" : "No") << endl;
 
     bst->removeBST(6);
-    cout << "Does the tree contain 6 after removal? " << (bst->searchBST(6) ? "Yes" : "No") << endl;
+    cout << "Does the tree contain 6 after removal? " << (bst->searchBST(66) ? "Yes" : "No") << endl;
 
     delete bst;
     return 0;
